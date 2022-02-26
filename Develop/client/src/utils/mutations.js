537a2acfc7ce4c0d8eba197697702fd637415baa
Connor <90352499+Connor-vb@users.noreply.github.com>
2022-02-25 17:00:30 -0800

    added init files

---
+++ Develop/client/src/utils/mutations.js
@@ -0,0 +1,55 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_BOOK = gql`
+  mutation addThought($thoughtText: String!) {
+    addThought(thoughtText: $thoughtText) {
+      _id
+      thoughtText
+      createdAt
+      username
+      reactionCount
+      reactions {
+        _id
+      }
+    }
+  }
+`;
+
+
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($id: ID!) {
+    removeBook(id: $id) {
+      _id
+      username
+      books {
+        _id
+        username
+      }
+    }
+  }
+`;
